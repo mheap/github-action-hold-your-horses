cd7c29038afe9d2c37d3c45adf5f590e7be2897e
Michael Heap <m@michaelheap.com>
2020-03-07 15:05:29 +0000

    Extract SHA from PRs

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ Toolkit.run(async tools => {
 
   if (tools.context.event == "pull_request" && ['opened', 'synchronize'].indexOf(tools.context.payload.action) !== -1) {
     // If the PR is opened or synchronized
-    await addPendingStatusCheck();
+    await addPendingStatusCheck(tools);
   } else {
     // It's run on schedule, so let's check if any statuses need to be updated
     tools.log.info("Schedule code");
+    const prs = (await tools.github.pulls.list({
+      ...tools.context.repo
+    })).data;
+
+    const shas = prs.map((pr) => {
+      return pr.head.sha
+    });
+
+    console.log(shas);
   }
   tools.exit.success("Action finished");
 })
